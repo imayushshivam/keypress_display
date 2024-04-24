33e2cb2b6607094fcfcfcc3ca6366436cc684563
imayushshivam <ayushshivampandey@gmail.com>
2024-04-24 10:55:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const inserField = document.getElementById("insert");
+
+window.addEventListener("keydown", function (e) {
+  inserField.innerHTML = `
+  <div class ='color'>
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>
+  </div>`;
+});
